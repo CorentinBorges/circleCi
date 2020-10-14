2afb57f24227eb5ce3a1aa1cb9825cef0006c5e2
CorentinBorges <56678169+CorentinBorges@users.noreply.github.com>
2020-10-14 15:58:19 +0200

    client can see his id on connection

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -25,7 +25,7 @@ class Client implements UserInterface
      * @var string
      * @ORM\Id()
      * @ORM\Column(type="string")
-     * @Groups({"list_all"})
+     * @Groups({"list_all", "on_connect"})
      */
     private $id;
 
@@ -33,7 +33,7 @@ class Client implements UserInterface
      * @var string
      *
      * @ORM\Column(type="string", length=64)
-     * @Groups({"details"})
+     * @Groups({"details","on_connect"})
      */
     private $username;
 
---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+
+use App\Entity\Client;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class AuthenticationSuccessListener
+{
+
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        /**
+         * @var Client $client
+         */
+        $client = $event->getUser();
+
+        if (!$client instanceof Client) {
+            return;
+        }
+
+        $data['client'] = array(
+            'id' => $client->getid(),
+        );
+
+        $event->setData($data);
+    }
+}
